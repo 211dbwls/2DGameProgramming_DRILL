23492e222b57397d3e495a1f7fa09d4e7f48c776
211dbwls <211dbwls@naver.com>
2021-10-03 23:24:04 +0900

    Project_1주차_배경

--- MarioBros_2018184021/Stage1.py
+++ MarioBros_2018184021/Stage1.py
@@ -2,6 +2,11 @@ from pico2d import *
 
 open_canvas()
 
-background = load_image('Resource/Background.png')
+background = load_image('Background.png')
+character = load_image('Mario.png')
 
-close_canvas()
+# clip_draw(
+character.clip_draw(50, 50)
+background.draw_now(0, 0)
+
+delay(10)
